a9e95f03f1dcca383a53f03ebba1b8d0131b168d
lcubas <langelcubas9@gmail.com>
2019-10-22 17:39:02 -0500

    add config to mimify images

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,20 @@ const gulp = require('gulp'),
     pug = require('gulp-pug'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin');
 
+// tarea para juntar todos los js y minificarlos en un solo arhivo que es: build/js/main.min.js
 gulp.task('js', () => {
-    return gulp.src(['src/js/bootstrap.js', 'src/js/*.js'])
+    return gulp.src(['js/jquery.js', 'src/js/*.js'])
             .pipe(concat('main.min.js'))
             .pipe(uglify())
             .pipe(gulp.dest('build/js/'));
 });
 
+// tarea para juntar todos los scss y minificarlos en un solo arhivo que es: build/css/main.min.css
 gulp.task('sass', () => {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src('scss/**/*.scss')
             .pipe(sass({
                 outputStyle: 'compressed',
                 errLogToConsole: true
@@ -26,18 +29,25 @@ gulp.task('sass', () => {
             .pipe(gulp.dest('build/css/'));
 });
 
+// tarea para compilar u¿y nuestro archivo index.pug convertirlo en index.html
 gulp.task('pug', () => {
     return gulp.src('*.pug')
-            .pipe(pug({
-                pretty: true
-            }))
+            .pipe(pug())
             .pipe(gulp.dest('build/'));
 });
 
+// tarea para comprimir y reducir el peso de nuestras imagenes
+gulp.task('image', () => {
+    return gulp.src('images/**/*.+(png|jpg|gif)')
+            .pipe(imagemin())
+            .pipe(gulp.dest('build/images'));
+ });
+
+ // tarea para que este constantemente vigilando las tareas definidas dentro
 gulp.task('watch', () => {
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
     gulp.watch('*.pug', gulp.series('pug'));
-    gulp.watch('src/js/**/*.js', gulp.series('js'));
+    gulp.watch('js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series('watch'));
